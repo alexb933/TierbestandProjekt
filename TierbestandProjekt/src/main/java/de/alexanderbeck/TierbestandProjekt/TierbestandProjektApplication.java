38e447751dff7bb928447808d64e4202e78b5b9c
alexb <alexb@DESKTOP-H89P201.fritz.box>
2019-12-18 20:50:18 +0100

    initial commit

---
+++ TierbestandProjekt/src/main/java/de/alexanderbeck/TierbestandProjekt/TierbestandProjektApplication.java
@@ -0,0 +1,13 @@
+package de.alexanderbeck.TierbestandProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierbestandProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierbestandProjektApplication.class, args);
+	}
+
+}
---
+++ TierbestandProjekt/src/test/java/de/alexanderbeck/TierbestandProjekt/TierbestandProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package de.alexanderbeck.TierbestandProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierbestandProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
