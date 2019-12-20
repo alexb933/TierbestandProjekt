ed553f8ba2e1c6e8328e65797ea2a0c8d68e3a82
alexb <alexb@DESKTOP-H89P201.fritz.box>
2019-12-21 00:15:17 +0100

    Tabellen fast angelegt. Datum funktioniert. Komlettbestände fehlen noch

--- TierbestandProjekt/src/main/java/de/alexanderbeck/TierbestandProjekt/Bestand.java
+++ TierbestandProjekt/src/main/java/de/alexanderbeck/TierbestandProjekt/Bestand.java
@@ -1,9 +1,13 @@
 package de.alexanderbeck.TierbestandProjekt;
 
+
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 
 @Entity
@@ -12,31 +16,83 @@ public class Bestand {
 	  @Id
 	  @GeneratedValue(strategy=GenerationType.AUTO)
 	  private Long id;
+	  private String bnummer;
+	  private LocalDateTime zeitstamp;
+	  private int nutzartcode;
+	  private String nutzarttxt;
+	  private String aenderartcode;
+	  private String aenderarttxt;
 	  private int anzahl;
+	  private int halbjahr;
 	  private String hinweis;
+//	  private int bestandu30;
+//	  private int bestandab30;
 
 	  protected Bestand() {}
 
-	  public Bestand(Integer anzahl, String hinweis) {
+	  public Bestand( String nutzarttxt, String aenderartcode, Integer anzahl, Integer halbjahr, String hinweis) {
+	    this.bnummer ="08 436 095 0018";
+	    
+	    this.nutzarttxt = nutzarttxt;
+	    if (nutzarttxt == "bis30kg") {
+	    	this.nutzartcode = 31;
+	    } else if (nutzarttxt == "ab30kg"){
+	    	this.nutzartcode = 32;
+	    } else {
+	    	this.nutzartcode = 0;
+	    	System.out.println("Error: Undefinierbare Nutzungsart");
+	    }
+	    
+	    this.aenderartcode = aenderartcode;
+	    if (aenderartcode == "BZU") {
+	    	this.aenderarttxt = "Bestandszugang";
+	    } else if (aenderartcode == "BAB") {
+	    	this.aenderarttxt = "Bestandsabgang";
+	    } else {
+	    	this.aenderarttxt = "Error";
+	    	System.out.println("Error: Undefinierbare Änderungsart");
+	    }
+	    
 	    this.anzahl = anzahl;
+	    this.halbjahr = halbjahr;
 	    this.hinweis = hinweis;
+	    this.zeitstamp = LocalDateTime.now();
 	  }
 
 	  @Override
 	  public String toString() {
 	    return String.format(
-	        "Bestand[id=%d, anzahl='%d', hinweis='%s']",
-	        id, anzahl, hinweis);
+	        "Bestand[id=%d, bnummer='%s', Zeit='%s', nutzartcode='%d', nutzarttxt='%s', aenderartcode='%s', aenderarttxt='%s', anzahl='%d', halbjahr='%d', hinweis='%s']",
+	        id, bnummer, zeitstamp, nutzartcode, nutzarttxt, aenderartcode, aenderarttxt, anzahl, halbjahr, hinweis);
 	  }
 
 	  public Long getId() {
 	    return id;
 	  }
-
+	  public String getBNummer() {
+		    return bnummer;
+		  }
+	  public LocalDateTime getZeitStamp() {
+		  return zeitstamp;
+		  }
+	  public int getNutzartCode() {
+		    return nutzartcode;
+		  }
+	  public String getNutzartTxt() {
+		    return nutzarttxt;
+		  }
+	  public String getAenderartCode() {
+		    return aenderartcode;
+		  }
+	  public String getAenderartTxt() {
+		    return aenderarttxt;
+		  }
 	  public int getAnzahl() {
 	    return anzahl;
 	  }
-
+	  public int getHalbjahr() {
+		    return halbjahr;
+		  }
 	  public String getHinweis() {
 	    return hinweis;
 	  }
--- TierbestandProjekt/src/main/java/de/alexanderbeck/TierbestandProjekt/TierbestandProjektApplication.java
+++ TierbestandProjekt/src/main/java/de/alexanderbeck/TierbestandProjekt/TierbestandProjektApplication.java
@@ -1,5 +1,7 @@
 package de.alexanderbeck.TierbestandProjekt;
 
+
+
 import org.aspectj.weaver.ast.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,12 +23,12 @@ public class TierbestandProjektApplication {
 	  public CommandLineRunner demo(BestandRepository repository) {
 	    return (args) -> {
 	      // save a few data
-	      repository.save(new Bestand(200, "Test Hinweis"));
-	      repository.save(new Bestand(200, "Test Hinweis2"));
-	      repository.save(new Bestand(300, "Test Hinweis3"));
+	      repository.save(new Bestand( "bis30kg", "BZU", 10, 192, "Test Hinweis"));
+	      repository.save(new Bestand( "ab30kg", "BAB", 20, 192, "Test Hinweis2"));
+	      repository.save(new Bestand( "bis30serkg", "res", 10, 192, "Test Hinweis3"));
 
 
-	      // fetch all bestand
+	      			// fetch all bestand
 	      log.info("Bestand found with findAll():");
 	      log.info("-------------------------------");
 	      for (Bestand bestand : repository.findAll()) {
@@ -34,22 +36,22 @@ public class TierbestandProjektApplication {
 	      }
 	      log.info("");
 
-	      // fetch an individual eintrag by ID
+	      			// fetch an individual eintrag by ID
 	      Bestand bestand = repository.findById(1L);
 	      log.info("Eintrag found with findById(1L):");
 	      log.info("--------------------------------");
 	      log.info(bestand.toString());
 	      log.info("");
 
-	      // fetch customers by hinweis
+	      			// fetch customers by hinweis
 	      log.info("Customer found with Hinweis('Test Hinweis'):");
 	      log.info("--------------------------------------------");
 	      repository.findByHinweis("Test Hinweis").forEach(hinweis -> {
 	        log.info(hinweis.toString());
 	      });
-	      // for (Customer bauer : repository.findByLastName("Bauer")) {
-	      //  log.info(bauer.toString());
-	      // }
+			      // for (Customer bauer : repository.findByLastName("Bauer")) {
+			      //  log.info(bauer.toString());
+			      // }
 	      log.info("");
 	    };
 	  }
